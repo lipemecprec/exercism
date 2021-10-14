98f405f76789e490f5c940bb2bfb97223e66b684
Felipe Aguilar <lipemecprec@yahoo.com.br>
2021-10-14 00:05:27 -0300

    bird-count

---
+++ ruby/bird-count/bird_count.rb
@@ -0,0 +1,27 @@
+# Birdwatcher notes
+class BirdCount
+  def self.last_week
+    [0, 2, 5, 3, 7, 8, 4]
+  end
+
+  def initialize(birds_per_day)
+    @birds_per_day = birds_per_day
+  end
+
+  def yesterday
+    @birds_per_day[-2]
+  end
+
+  def total
+    @birds_per_day.sum
+  end
+
+  def busy_days
+    @birds_per_day.count { |birds_at_day| birds_at_day >= 5 }
+  end
+
+  def day_without_birds?
+    no_birds_day = @birds_per_day.count(&:zero?)
+    !no_birds_day.zero?
+  end
+end
